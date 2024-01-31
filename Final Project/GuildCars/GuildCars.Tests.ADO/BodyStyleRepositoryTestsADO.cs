81196d2020640006c9676d5191bee0bbdbfacdd0
TheCoberto <coby.kelly.ck@gmail.com>
2024-01-31 11:51:12 -0500

    setup db for BodyStyle tests

--- Final Project/GuildCars/GuildCars.Tests.ADO/BodyStyleRepositoryTestsADO.cs
+++ Final Project/GuildCars/GuildCars.Tests.ADO/BodyStyleRepositoryTestsADO.cs
@@ -1,7 +1,11 @@
 ﻿using GuildCars.Data.Repositories.ADO;
 using GuildCars.Models.Tables;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 
 namespace GuildCars.Tests.BodyStyleRepositoryTests
@@ -9,6 +13,45 @@ namespace GuildCars.Tests.BodyStyleRepositoryTests
     [TestFixture]
     public class BodyStyleRepositoryTestsADO
     {
+        [SetUp]
+        public void Init()
+        {
+            var dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+            try
+            {
+                using (dbConnection)
+                {
+                    var cmd = new SqlCommand
+                    {
+                        CommandText = "CarhallaDbReset",
+                        CommandType = System.Data.CommandType.StoredProcedure,
+
+                        Connection = dbConnection
+                    };
+                    dbConnection.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = String.Format(CultureInfo.CurrentCulture,
+                          "Exception Type: {0}, Message: {1}{2}",
+                          ex.GetType(),
+                          ex.Message,
+                          ex.InnerException == null ? String.Empty :
+                          String.Format(CultureInfo.CurrentCulture,
+                                       " InnerException Type: {0}, Message: {1}",
+                                       ex.InnerException.GetType(),
+                                       ex.InnerException.Message));
+
+                System.Diagnostics.Debug.WriteLine(errorMessage);
+
+                dbConnection.Close();
+            }
+        }
+
         [Test]
         public void CanGetAllBodyStyles()
         {
